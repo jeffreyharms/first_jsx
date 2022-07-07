af629632de65c90b302d04ebbbe4a7535854b8b0
Jeff Harms <jeffdharms@gmail.com>
2022-07-07 11:46:37 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello Dojo!</h1>
+        <h3>Things I Need To Do:</h3>
+        <ul>
+          <li>Learn React</li>
+          <li>Die on Mt. Everest</li>
+          <li>Collapse during marathon</li>
+          <li>Feed myself to dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
